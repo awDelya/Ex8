5e8b252d6caad7ae8a50a8b83f1f0057daf3744a
Овчинникова Аделина <deley@mail.ru>
2019-06-20 00:46:13 +0500

    Add function CheckTable

--- DerevoLi/DerevoLi/Program.cs
+++ DerevoLi/DerevoLi/Program.cs
@@ -12,6 +12,9 @@ namespace DerevoLi
         private static Random rnd = new Random();
         private static List<int> fTop, sTop;
         private static int[,] matrix;
+        private static int kol = 0;
+        private static bool Repite = false, find = true;
+        private static bool[] number;
         private static void PrintTable(int kolTop)
         {
             Color.Print(" ---------------------------------\n", ConsoleColor.Green);
@@ -34,11 +37,11 @@ namespace DerevoLi
                     for (int i = 0; i < kolTop; i++)
                     {
                         Color.Print("\n\n Введите 1 вершину у " + (i + 1) + " ребра: ");
-                        fTop[i] = Number.Check(1, kolTop);
+                        fTop[i] = Number.Check(1, kolTop + 1);
                         Console.Clear();
                         PrintTable(kolTop);
                         Color.Print("\n\n Введите 2 вершину у " + (i + 1) + " ребра: ");
-                        sTop[i] = Number.Check(1, kolTop);
+                        sTop[i] = Number.Check(1, kolTop + 1);
                         Console.Clear();
                         PrintTable(kolTop);
                     }
@@ -47,8 +50,8 @@ namespace DerevoLi
                     Console.Clear();
                     for (int i = 0; i < kolTop; i++)
                     {
-                        fTop[i] = rnd.Next(1, kolTop+1);
-                        sTop[i] = rnd.Next(1, kolTop+1);
+                        fTop[i] = rnd.Next(1, kolTop + 1);
+                        sTop[i] = rnd.Next(1, kolTop + 1);
                     }
                     PrintTable(kolTop);
                     break;
@@ -78,6 +81,79 @@ namespace DerevoLi
             }
             matrix = GetIncidenceMatrix(edges);
         }
+        private static void CheckTable(int kolTop)
+        {
+            /********************************************************///Блок для find(проверка на наличие всех вершин)
+            number = new bool[kolTop];
+            for (int i = 1; i < kolTop + 1; i++)
+            {
+                if (fTop.Contains(i) || sTop.Contains(i))
+                    number[i - 1] = true;
+            }
+            for (int i = 0; i < number.Length; i++)
+            {
+                find = find && number[i];
+            }
+            /********************************************************///Блок Repite(проверка повторюшек (+циклы?))
+            for (int i = 0; i < fTop.Count; i++)
+            {
+                string last, now, rev;
+                last = fTop[i] + " " + sTop[i];
+                try
+                {
+                    now = fTop[i + 1] + " " + sTop[i + 1];
+                    rev = fTop[i + 1] + " " + sTop[i + 1];
+                    rev = new string(rev.ToCharArray().Reverse().ToArray());
+                    if (last == now || last == rev)
+                    {
+                        Repite = true;
+                        break;
+                    }
+                }
+                catch (ArgumentOutOfRangeException) { }
+            }
+            /********************************************************///Проверка дерева на связность(Все ли вершины связанны ребрами)
+            if (!Repite && find)
+            {
+                Matrix(kolTop);
+                Queue<int> q = new Queue<int>();    //Это очередь, хранящая номера вершин
+                Queue<int> q2 = new Queue<int>();
+                for (int i = 0; i < kolTop + 1; i++)
+                    for (int j = 0; j < kolTop; j++)
+                        q2.Enqueue(matrix[i, j]);
+                int u = kolTop;
+                bool[] used = new bool[u + 1];  //массив отмечающий посещённые вершины
+                int[][] g = new int[u + 1][];   //массив содержащий записи смежных вершин
+                for (int i = 0; i < u + 1; i++)
+                {
+                    g[i] = new int[u];
+                    for (int j = 0; j < u; j++)
+                    {
+                        g[i][j] = q2.Dequeue();
+                    }
+                }
+                used[u] = true;     //массив, хранящий состояние вершины(посещали мы её или нет)
+                q.Enqueue(u);
+                while (q.Count != 0)
+                {
+                    u = q.Peek();
+                    q.Dequeue();
+                    kol++;
+                    for (int i = 0; i < g.Length - 1; i++)
+                    {
+                        if (Convert.ToBoolean(g[u][i]))
+                        {
+                            if (!used[i])
+                            {
+                                used[i] = true;
+                                q.Enqueue(i);
+                            }
+                        }
+                    }
+                }
+            }
+            /********************************************************/
+        }
         static void Main()
         {
             Color.Print("\n Введите колличество ребер (от 2 до 9): ", ConsoleColor.Yellow);
@@ -89,7 +165,12 @@ namespace DerevoLi
                 fTop.Add(0);
                 sTop.Add(0);
             }
-            DoTable(kolTop);            
+            DoTable(kolTop);
+            CheckTable(kolTop);
+            if (kol == kolTop + 1)
+                Color.Print("\n Дерево существует!\n", ConsoleColor.Green);
+            else
+                Color.Print("\n Дерево не существует\n", ConsoleColor.Red);
             Text.GoBackMenu();
         }
     }
